5ebe14403c31c66551e464dd4fd675f027c9cdb7
ArturoEscamilla7 <72032838+ArturoEscamilla7@users.noreply.github.com>
2021-07-17 13:51:50 -0500

    add service

---
+++ Source/Jamsito/Blackboard/Service/CheckDistanceService.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "CheckDistanceService.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JAMSITO_API UCheckDistanceService : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
